c9f0c885bec15a10c550a2a4b90c20f48896b22d
casterfile <dev.anthonycastor@gmail.com>
2017-05-29 12:51:12 +0800

    Facebook Registration

--- BookMD/ViewController.swift
+++ BookMD/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+     var dict : NSDictionary!
+    
+     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["email", "public_profile"]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func LoginButton(_ sender: Any) {
+        performSegue(withIdentifier: "MainBookMD", sender: self)
+    }
+    
+    @IBAction func RegistrationButton(_ sender: Any) {
+        performSegue(withIdentifier: "Registration", sender: self)
+    }
+    @IBAction func FBLogin(_ sender: Any) {
+        FBSDKLoginManager().logIn(withReadPermissions:["email", "public_profile"], from: self){
+            (result, err) in
+            if err != nil {
+                print("Custom FB Login failed", err)
+                return
+            }
+            
+            self.ShowEmailAddress()
+        }
+
+    }
+    
+    func ShowEmailAddress() {
+        print("Successfully logged in with facebook")
+        //Grabbing the FB User Info
+        FBSDKGraphRequest(graphPath:"/me", parameters:["fields": "id, name, first_name, last_name, picture.type(large), email"]).start{
+            (connection, result,err) in
+            if err != nil {
+                print("Failed to start graph request:", err)
+            }
+            
+            if let data = result as? [String:Any] {
+                print(data["id"]!)
+                print(data["email"]!)
+                print(data["first_name"]!)
+                print(data["last_name"]!)
+            }
+            
+            if(result != nil){
+                self.performSegue(withIdentifier: "MainBookMD", sender: self)
+            }
+            
+        }
+    }
+
+    
 
 }
 
